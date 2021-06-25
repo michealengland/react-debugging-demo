d6792ad04b437afb6b9ee5c74c0f647ff8218d3f
Mike England <mike.england@webdevstudios.com>
2021-06-25 13:51:20 -0400

    Setup a broken promise to return invalid PropTypes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 import Paragraph from './examples/example-1'
 import InfiniteCounter from './examples/example-2'
 import AnimalKingdom from './examples/example-3';
+import Giraffes from './examples/example-4';
 
 function App() {
+	const [giraffeCount, setGiraffeCount] = useState(null);
+
+  useEffect(() => {
+    const fetchGiraffeCount = () => new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve('20');
+      }, 600);
+    })
+    .then((result) => {
+      setGiraffeCount(result)
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+    if (giraffeCount === null) {
+      fetchGiraffeCount();
+    }
+  }, [giraffeCount]);
+
   return (
     <div className="App">
       {/* <Paragraph
@@ -13,7 +35,8 @@ function App() {
         }}
       /> */}
       {/* <InfiniteCounter /> */}
-      <AnimalKingdom />
+      {/* <AnimalKingdom /> */}
+      <Giraffes numberOfGiraffes={giraffeCount} />
     </div>
   );
 }
