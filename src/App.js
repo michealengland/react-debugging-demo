c6f57d8dde24458b4b37aee12fca6919fa9819e3
Mike England <mike.england@webdevstudios.com>
2021-06-25 12:26:14 -0400

    Setup expected a rerender error

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import Paragraph from './examples/example-1'
+import InfiniteCounter from './examples/example-2'
+import AnimalKingdom from './examples/example-3';
 
 function App() {
   return (
     <div className="App">
-      <Paragraph
+      {/* <Paragraph
         textObj={{
             word1: 'hello',
             word2: 'goodbye',
         }}
-      />
+      /> */}
+      {/* <InfiniteCounter /> */}
+      <AnimalKingdom />
     </div>
   );
 }
---
+++ src/examples/example-3.js
@@ -0,0 +1,20 @@
+/**
+ * Try:
+ * Feed the hungry animal.
+ */
+export default function AnimalKingdom() {
+	let energy = 0;
+
+	const eat = () => energy += 10;
+
+	return (
+		<div>
+			<p>Animal Energy: {energy}</p>
+			<button onClick={eat}>Feed the Animal</button>
+			{ energy >= 10 ?
+				<p>The animal is full!</p> :
+				<p>The animal is hungry!</p>
+			}
+		</div>
+	)
+}
