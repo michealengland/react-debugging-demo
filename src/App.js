2a35188acb6e3256bc2d6d24d3f7c3773287cb7d
johnrobertmcc <john.robert.mcc@gmail.com>
2021-06-28 14:01:40 -0400

    persisting state onr efresh

--- src/App.js
+++ src/App.js
@@ -1,33 +1,67 @@
-import './App.scss';
-import React, { useState } from 'react';
+import "./App.scss";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import Nav from './components/Navbar/Nav';
-import Instructions from './components/Instructions';
-import Dashboard from './components/Dashboard';
-import Footer from './components/Footer';
+import Nav from "./components/Navbar/Nav";
+import Instructions from "./components/Instructions";
+import Dashboard from "./components/Dashboard";
+import Footer from "./components/Footer";
 
 function App() {
-  const defaultState = {one: false, two:false, three: false, four: false, five: false, six: false, seven: false}
-  const [example, setExample] = useState()
-  const [completed, setCompleted] = useState(defaultState)
-  const [modal, setModal] = useState(false)
+  const defaultState = {
+    one: false,
+    two: false,
+    three: false,
+    four: false,
+    five: false,
+    six: false,
+    seven: false,
+  };
+  const [example, setExample] = useState();
+  const [completed, setCompleted] = useState(defaultState);
+  const [modal, setModal] = useState(false);
+
+  useEffect(() => {
+    const prevCompleted = localStorage.getItem("completed");
+    const prevExample = localStorage.getItem("example");
+    if (prevCompleted) {
+      setCompleted(JSON.parse(prevCompleted));
+      setExample(prevExample);
+    } else {
+      setCompleted(defaultState);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("completed", JSON.stringify(completed));
+  }, [completed]);
+  useEffect(() => {
+    localStorage.setItem("example", example);
+  }, [example]);
+
   const openModal = () => {
-    setModal(true)
+    setModal(true);
   };
 
   const closeModal = () => {
-    setModal(false)
-  }
+    setModal(false);
+  };
 
-   return (
-    <Router >
+  return (
+    <Router>
       <div className="App">
-        <Nav completed={completed} setExample={setExample}/>
-        <div className='example-outr'>
-          <Instructions example={example} setCompleted={setCompleted}/>
-          <Dashboard setCompleted={setCompleted}/>
+        <Nav completed={completed} setExample={setExample} />
+        <div className="example-outr">
+          <Instructions example={example} setCompleted={setCompleted} />
+          <Dashboard setCompleted={setCompleted} />
         </div>
-         <Footer completed={completed} openModal={openModal} closeModal={closeModal} modal={modal} setCompleted={setCompleted} defaultState={defaultState}/>
+        <Footer
+          completed={completed}
+          openModal={openModal}
+          closeModal={closeModal}
+          modal={modal}
+          setCompleted={setCompleted}
+          defaultState={defaultState}
+        />
       </div>
     </Router>
   );
