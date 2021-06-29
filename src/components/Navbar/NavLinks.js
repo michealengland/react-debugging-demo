6f4bb60828db94fdbac0b3ea972931a703e2c807
johnrobertmcc <john.robert.mcc@gmail.com>
2021-06-29 14:31:24 -0400

    changed ul to nav, refactored class name to complete/incomplete

--- src/components/Navbar/NavLinks.js
+++ src/components/Navbar/NavLinks.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
-import { PropTypes } from 'prop-types';
+import {PropTypes} from 'prop-types'
 
 export default function NavLinks({
   completed,
@@ -10,7 +10,7 @@ export default function NavLinks({
   setExample,
 }) {
   return (
-    <ul className={`${outerClassName} nav`} onClick={handleClick}>
+    <nav className={`${outerClassName} nav`} onClick={handleClick}>
       <NavLink
         to="/"
         exact
@@ -21,54 +21,68 @@ export default function NavLinks({
       </NavLink>
       <NavLink
         to="/example1"
-        className={`${innerClassName} ${completed.one}`}
+        className={`${innerClassName} ${
+          completed.one ? 'complete' : 'incomplete'
+        }`}
         onClick={() => setExample('one')}
       >
         Paragraph
       </NavLink>
       <NavLink
         to="/example2"
-        className={`${innerClassName} ${completed.two}`}
+        className={`${innerClassName} ${
+          completed.two ? 'complete' : 'incomplete'
+        }`}
         onClick={() => setExample('two')}
       >
         Infinite Counter
       </NavLink>
       <NavLink
         to="/example3"
-        className={`${innerClassName} ${completed.three}`}
+        className={`${innerClassName} ${
+          completed.three ? 'complete' : 'incomplete'
+        }`}
         onClick={() => setExample('three')}
       >
         Feed the Animals
       </NavLink>
       <NavLink
         to="/example4"
-        className={`${innerClassName} ${completed.four}`}
+        className={`${innerClassName} ${
+          completed.four ? 'complete' : 'incomplete'
+        }`}
         onClick={() => setExample('four')}
       >
         Count the Giraffes
       </NavLink>
       <NavLink
         to="/example5"
-        className={`${innerClassName} ${completed.five}`}
+        className={`${innerClassName} ${
+          completed.five ? 'complete' : 'incomplete'
+        }`}
         onClick={() => setExample('five')}
       >
         Rename the Elephant
       </NavLink>
       <NavLink
         to="/example6"
-        className={`${innerClassName} ${completed.six}`}
+        className={`${innerClassName} ${
+          completed.six ? 'complete' : 'incomplete'
+        }`}
         onClick={() => setExample('six')}
       >
         Fix the Tiger
       </NavLink>
       <NavLink
         to="/example7"
-        className={`${innerClassName} ${completed.seven}`}
+        className={`${innerClassName} ${
+          completed.seven ? 'complete' : 'incomplete'
+        }`}
         onClick={() => setExample('seven')}
       >
         Range Slider
       </NavLink>
-    </ul>
+    </nav>
   )
 }
 
@@ -78,5 +92,4 @@ NavLinks.propTypes = {
   innerClassName: PropTypes.string,
   outerClassName: PropTypes.bool,
   setExample: PropTypes.func,
-
 }
