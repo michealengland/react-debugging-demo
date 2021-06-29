5620eb46b5d91c583d7bc20b8df74f332ce3d651
Mike England <mike.england@webdevstudios.com>
2021-06-28 23:45:36 -0400

    Fix linter errors on example-1

--- src/examples/example-1.js
+++ src/examples/example-1.js
@@ -1,12 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
 
 /**
  * Try:
  * Fix the missing value and render a sentence.
  */
 export default function Paragraph({textObj}) {
-    const {word1, word2, word3} = textObj
+  const {word1, word2, word3} = textObj
 
-    return (
-        <p>{`${word1} + ${word2} + ${word3.value}`}</p>
-    );
+  return <p>{`${word1} + ${word2} + ${word3.value}`}</p>
+}
+
+Paragraph.propTypes = {
+  textObj: PropTypes.shape({
+    word1: PropTypes.string,
+    word2: PropTypes.string,
+    word3: PropTypes.string,
+  })
 }
