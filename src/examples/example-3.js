b0f34e57c4803980361cb2d66a115ad0a75fd63d
Mike England <mike.england@webdevstudios.com>
2021-06-28 23:46:54 -0400

    Fix example-3 linter errors

--- src/examples/example-3.js
+++ src/examples/example-3.js
@@ -1,20 +1,18 @@
+import React from 'react'
 /**
  * Try:
  * Feed the hungry animal.
  */
 export default function AnimalKingdom() {
-	let energy = 0;
+  let energy = 0
 
-	const eat = () => energy += 10;
+  const eat = () => (energy += 10)
 
-	return (
-		<div>
-			<p>Animal Energy: {energy}</p>
-			<button onClick={eat}>Feed the Animal</button>
-			{ energy >= 10 ?
-				<p>The animal is full!</p> :
-				<p>The animal is hungry!</p>
-			}
-		</div>
-	)
+  return (
+    <div>
+      <p>Animal Energy: {energy}</p>
+      <button onClick={eat}>Feed the Animal</button>
+      {energy >= 10 ? <p>The animal is full!</p> : <p>The animal is hungry!</p>}
+    </div>
+  )
 }
